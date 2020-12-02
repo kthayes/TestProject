a1762e6f2537f4769f833ef19d570f3e21de6b50
hayes <hayes@DT-NIGHTHAWK>
2020-12-02 09:48:51 -0600

    Completed

--- MiscProjects/PigLatin.py
+++ MiscProjects/PigLatin.py
@@ -1,28 +1,53 @@
+# Dictionary of vowels
+VOWELS = {"a", "e", "i", "o", "u", "y"}
 
+# Takes a line of words and converts them to Pig Latin
 def translate(line):
-    return "test"
+    tr = ""
+    # Make a list of each word in the line
+    words = line.split()
+    # Step through each word in the list
+    for word in words:
+        # Check if the first letter is a vowel, if it is, add "yay" to end of the word
+        if(word[0] in VOWELS):
+            word += "yay "
+        # If the first letter is not a vowel, then it is a consonant
+        else:
+            # Step through each letter in the current word
+            for letter in word:
+                # If the current letter is a consonant, remove it from the front of the word, and place it at the end of the word
+                if(letter not in VOWELS):
+                    word = word[1:] + letter
+                # If we hit a vowel, we are done moving consonants
+                else:
+                    break
+            # Add "ay" to the end of the word
+            word += "ay "
+        # Add the translated word to the sentence
+        tr += word
+    # Use .strip() on the return to eliminate the trailing " "
+    return tr.strip()
 
+# "main" function, called at the bottom
 def PigLatin():
     # Change fName to match the file name to be inputed
     fName = "lines.txt"
     fDir = "PigLatin_InputFiles/"
     fDir += fName
-    print("")
     
+    # try-except to catch non-exiting or corrupt files
     try:
-        f = open(fName, "r")
+        f = open(fDir, "r")
     except FileNotFoundError:
         print("Unable to open input file.")
         return
 
+    # Store all lines in the file in a list
     lines = f.readlines()
     f.close()
     
-    if(len(lines) == 0):
-        return
-    
+    # Call translate() function for each line in the list, and print the result
     for line in lines:
-        translated = translate(line)
-        print(translated)
+        print(translate(line.strip()))
         
 PigLatin()
